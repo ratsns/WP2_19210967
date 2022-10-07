dee3b0c1025e24019e249de1120fe6480f9f279a
ratsns <ratrinur3322@gmail.com>
2022-10-07 18:18:26 +0700

    membuat templating website sederhana

---
+++ application/controllers/Web.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit ('no direct script access allowed');
+
+class Web extends CI_Controller{
+	function __construct(){
+	parent::__construct();
+ }
+ 
+public function index() {
+	 $data['judul'] = "Halaman Depan";
+	 //$this->load->view('web_template/v_header',$data);
+	 //$this->load->view('web_template/v_index',$data)
+	 //$this->load->view('web_template/v_footer',$data);
+	 $this->load->view('web_template/v_template',$data);
+ }
+ public function about()
+ {
+	$data['judul'] = "Halaman About";
+	$this->load->view('web_template/v_header',$data);
+	$this->load->view('web_template/v_about',$data);
+	$this->load->view('web_template/v_footer',$data);
+ }
+}
