15110f429fa93287ddfd77a038b8979f9b0e057e
ratsns <ratrinur3322@gmail.com>
2022-10-14 13:30:38 +0700

    membuat form validasi

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -7,11 +7,25 @@ class Matakuliah extends CI_Controller
  }
  public function cetak()
  {
-	$data = [
-		'kode' => $this->input->post('kode'),
-		'nama' => $this->input->post('nama'),
-		'sks' => $this->input->post('sks')
+	$this->form_validation->set_rules('kode', 'Kode Matakuliah', 
+	'required|min_length[3]', ['required' => 'Kode Matakuliah Harus 
+	diisi', 'min_length' => 'Kode terlalu pendek']);
+	$this->form_validation->set_rules('nama', 'Nama Matakuliah', 
+	'required|min_length[3]', ['required' => 'Nama Matakuliah Harus
+	diisi','min_length' => 'Nama terlalu pendek']);
+	$this->form_validation->set_rules('sks', 'sks',
+	'required|min_length[0]', ['required' => 'SKS Harus Diisi',
+	'min_length' => 'SKS terlalu pendek']);
+	
+	if ($this->form_validation->run() != true) {
+		$this->load->view('view-form-matakuliah');
+	} else {
+		$data = [
+			'kode' => $this->input->post('kode'),
+			'nama' => $this->input->post('nama'),
+			'sks' => $this->input->post('sks')
 	];
 	$this->load->view('view-data-matakuliah', $data);
+	}
  }
 }
